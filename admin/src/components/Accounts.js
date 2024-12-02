165f5f65b659f2aa8c7db7d4cf368a0e566a2fa2
Arystan-Aldiyev <aldiyev.ars@gmail.com>
2024-12-03 02:26:56 +0500

    all

--- admin/src/components/Accounts.js
+++ admin/src/components/Accounts.js
@@ -1,31 +1,89 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, act } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../AuthContext';
+import ActiveTable from './ActiveTable';
 import PendingTable from './PendingTable';
 import SearchComponent from './SearchComponent';
 
 const Accounts = () => {
     const { logout } = useAuth();
+    const [users, setUsers] = useState([]);
+    // const [pending, setPending] = useState([]);
     const [active, setActive] = useState('pending');
     const [searchQuery, setSearchQuery] = useState('');
     const navigate = useNavigate();
+
+    // Fetch farmers data from the backend
+    useEffect(() => {
+        const fetchFarmers = async () => {
+            try {
+                const accessToken = localStorage.getItem('access');
+
+                await fetch('http://localhost:8000/auth/admin/users/', {
+                    method: 'GET',
+                    headers: { 'Content-Type': 'application/json', 'Authorization': 'Bearer ' + accessToken }
+                }).then((response) => {
+                    if (response && response.status === 200) {
+                        response.json().then((data) => {
+                            console.log(data);
+                            setUsers(data);
+                        })
+                    }
+                })
+            } catch (error) {
+                console.error('Error fetching farmers:', error);
+            }
+        };
+
+        fetchFarmers();
+        // setFarmers([
+        //     {
+        //         id: 1,
+        //         photo: "aboba",
+        //         name: 'Farmer 1',
+        //         farmSize: 200,
+        //         location: "Astana",
+        //         email: "farmer1@gmail.com",
+        //         phone: "87007007000"
+        //     },
+        //     {
+        //         id: 2,
+        //         photo: "aboba",
+        //         name: 'Farmer 2',
+        //         farmSize: 200,
+        //         location: "Astana",
+        //         email: "farmer1@gmail.com",
+        //         phone: "87007007000"
+        //     },
+        //     {
+        //         id: 3,
+        //         photo: "aboba",
+        //         name: 'Farmer 4',
+        //         farmSize: 200,
+        //         location: "Astana",
+        //         email: "farmer1@gmail.com",
+        //         phone: "87007007000"
+        //     },
+        //     {
+        //         id: 4,
+        //         photo: "aboba",
+        //         name: 'Farmer 4',
+        //         farmSize: 200,
+        //         location: "Astana",
+        //         email: "farmer1@gmail.com",
+        //         phone: "87007007000"
+        //     }
+        // ])
+    }, [active]);
 
     const handleLogout = () => {
         logout();
         navigate('/login');
     };
 
-    const farmers = [
-        'Alice Johnson',
-        'Bob Smith',
-        'Charlie Brown',
-        'Daisy Miller',
-        'Evelyn Harper',
-    ];
-
-    const filteredFarmers = farmers.filter((farmer) =>
-        farmer.toLowerCase().includes(searchQuery.toLowerCase())
-    );
+    // const filteredFarmers = farmers.filter((farmer) =>
+    //     farmer.toLowerCase().includes(searchQuery.toLowerCase())
+    // );
 
     return <div>
         <h1>Accounts</h1>
@@ -43,7 +101,9 @@ const Accounts = () => {
             placeholder="Search farmers..."
             onSearch={(query) => setSearchQuery(query)}
         />
-        <PendingTable />
+        {active === 'pending' && <PendingTable users={users} setActive={setActive} />}
+        {active === 'active' && <ActiveTable users={users} active={true} setActive={setActive} />}
+        {active === 'disabled' && <ActiveTable users={users} active={false} setActive={setActive} />}
     </div>;
 };
 
--- admin/src/components/PendingTable.js
+++ admin/src/components/PendingTable.js
@@ -1,69 +1,33 @@
 import React, { useState, useEffect } from 'react';
 
-const PendingTable = () => {
-  const [farmers, setFarmers] = useState([]);
-  const [loading, setLoading] = useState(true);
+const PendingTable = ({ users, setActive }) => {
+  const accessToken = localStorage.getItem('access');
 
-  // Fetch farmers data from the backend
-  useEffect(() => {
-    const fetchFarmers = async () => {
-      try {
-        const response = await fetch('https://example.com/api/farmers'); // Replace with your backend URL
-        const data = await response.json();
-        setFarmers(data);
-      } catch (error) {
-        console.error('Error fetching farmers:', error);
-      } finally {
-        setLoading(false);
-      }
-    };
+  let pending = users.filter((user) =>
+    user.role === 'farmer' && user.is_verified === false
+  );
 
-    // fetchFarmers();
-    setFarmers([
-      {
-        id: 1,
-        photo: "aboba",
-        name: 'Farmer 1',
-        farmSize: 200,
-        location: "Astana",
-        email: "farmer1@gmail.com",
-        phone: "87007007000"
-      },
-      {
-        id: 2,
-        photo: "aboba",
-        name: 'Farmer 2',
-        farmSize: 200,
-        location: "Astana",
-        email: "farmer1@gmail.com",
-        phone: "87007007000"
-      },
-      {
-        id: 3,
-        photo: "aboba",
-        name: 'Farmer 4',
-        farmSize: 200,
-        location: "Astana",
-        email: "farmer1@gmail.com",
-        phone: "87007007000"
-      },
-      {
-        id: 4,
-        photo: "aboba",
-        name: 'Farmer 4',
-        farmSize: 200,
-        location: "Astana",
-        email: "farmer1@gmail.com",
-        phone: "87007007000"
-      }
-    ])
-  }, []);
-
-  
-
-  // if (loading) {
-  //   return <p>Loading farmers...</p>;
-  // }
+  const approveAcc = async (farmer) => {
+    try {
+      await fetch(`http://localhost:8000/auth/admin/approve-farmer/`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json', 'Authorization': 'Bearer ' + accessToken },
+        body: JSON.stringify({
+          "email": farmer.email
+        })
+      }).then((response) => {
+        if (response && response.status === 200) {
+          console.log('success');
+          pending = users.filter((user) =>
+            user.role === 'farmer' && user.is_verified === false
+          );
+          setActive('pending')
+        }
+      })
+    } catch (error) {
+      console.error('Error fetching farmers:', error);
+    }
+  }
 
   return (
     <div style={{ padding: '20px' }}>
@@ -79,7 +43,7 @@ const PendingTable = () => {
         <thead>
           <tr>
             <th style={{ padding: '10px', borderBottom: '2px solid #ddd' }}>Photo preview</th>
-            <th style={{ padding: '10px', borderBottom: '2px solid #ddd' }}>Name</th>
+            <th style={{ padding: '10px', borderBottom: '2px solid #ddd' }}>Username</th>
             <th style={{ padding: '10px', borderBottom: '2px solid #ddd' }}>Farm size</th>
             <th style={{ padding: '10px', borderBottom: '2px solid #ddd' }}>Farm location</th>
             <th style={{ padding: '10px', borderBottom: '2px solid #ddd' }}>Email</th>
@@ -88,7 +52,7 @@ const PendingTable = () => {
           </tr>
         </thead>
         <tbody>
-          {farmers.map((farmer) => (
+          {pending.map((farmer) => (
             <tr key={farmer.id}>
               <td style={{ padding: '10px', borderBottom: '1px solid #ddd' }}>
                 <img
@@ -97,11 +61,11 @@ const PendingTable = () => {
                   style={{ width: '50px', height: '50px', borderRadius: '50%' }}
                 />
               </td>
-              <td style={{ padding: '10px', borderBottom: '1px solid #ddd' }}>{farmer.name}</td>
+              <td style={{ padding: '10px', borderBottom: '1px solid #ddd' }}>{farmer.username}</td>
               <td style={{ padding: '10px', borderBottom: '1px solid #ddd' }}>{farmer.farmSize}</td>
               <td style={{ padding: '10px', borderBottom: '1px solid #ddd' }}>{farmer.location}</td>
               <td style={{ padding: '10px', borderBottom: '1px solid #ddd' }}>{farmer.email}</td>
-              <td style={{ padding: '10px', borderBottom: '1px solid #ddd' }}>{farmer.phone}</td>
+              <td style={{ padding: '10px', borderBottom: '1px solid #ddd' }}>{farmer.phone_number}</td>
               <td style={{ padding: '10px', borderBottom: '1px solid #ddd' }}>
                 <button
                   style={{
@@ -112,8 +76,9 @@ const PendingTable = () => {
                     borderRadius: '5px',
                     cursor: 'pointer',
                   }}
+                  onClick={() => approveAcc(farmer)}
                 >
-                  View
+                  Approve
                 </button>
               </td>
             </tr>
