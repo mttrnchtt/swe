74547eab66daf784f55ada14d6f59106aaa20426
alibi <tolebayalibi24@gmail.com>
2024-12-03 09:49:24 +0600

    fix: fixed get product permissions for non-farmers

--- backend/apps/marketplace/urls.py
+++ backend/apps/marketplace/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from .views import CartListCreateAPIView, OrderListCreateAPIView
+from .views import (
+    CartListCreateAPIView,
+    OrderListCreateAPIView,
+    ProductListAPIView,
+)
 
 
 urlpatterns = [
     path('cart/', CartListCreateAPIView.as_view(), name='cart'),
     path('orders/', OrderListCreateAPIView.as_view(), name='orders'),
+    path('products/', ProductListAPIView.as_view(), name='products'),
 ]
--- backend/apps/marketplace/views.py
+++ backend/apps/marketplace/views.py
@@ -1,6 +1,8 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from .models import Cart, Order
+from apps.farm.models import Product
+from apps.farm.serializers import ProductSerializer
 from .serializers import (
     CartItemSerializer,
     CartCreateUpdateSerializer,
@@ -33,3 +35,29 @@ class OrderListCreateAPIView(generics.ListCreateAPIView):
 
     def get_queryset(self):
         return Order.objects.filter(user=self.request.user)
+    
+
+class ProductListAPIView(generics.ListAPIView):
+    """
+    Retrieve all available products with optional filtering by name and category.
+    """
+    permission_classes = [IsAuthenticated,]  # Open access for all users
+    serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        """
+        Filters available products by name, category, or other parameters.
+        """
+        queryset = Product.objects.all()
+
+        # Optional Filters
+        name_query = self.request.query_params.get('name', None)
+        category_query = self.request.query_params.get('category', None)
+
+        if name_query:
+            queryset = queryset.filter(name__icontains=name_query)
+
+        if category_query:
+            queryset = queryset.filter(category=category_query)
+
+        return queryset
